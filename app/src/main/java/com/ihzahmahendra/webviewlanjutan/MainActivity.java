7267db85dbd608650f36fd691cad01f8cde3a29e
ihza6661 <ihzahmahendra6661@gmail.com>
2026-04-13 01:52:51 +0700

    Initial commit: WebView Lanjutan Task

---
+++ app/src/main/java/com/ihzahmahendra/webviewlanjutan/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ihzahmahendra.webviewlanjutan;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        WebView myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+
+        myWebView.loadUrl("file:///android_asset/webview_lanjutan.html");
+    }
+}
---
+++ app/src/main/java/com/ihzahmahendra/webviewlanjutan/WebAppInterface.java
@@ -0,0 +1,35 @@
+package com.ihzahmahendra.webviewlanjutan;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class WebAppInterface {
+    Context mContext;
+
+    WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void showSms() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING);
+        mContext.startActivity(intent);
+    }
+
+    @JavascriptInterface
+    public void showWhatsApp(String target, String message) {
+        String url = "https://api.whatsapp.com/send?phone=" + target + "&text=" + message;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        mContext.startActivity(i);
+    }
+}
